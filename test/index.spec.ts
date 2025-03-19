2ab0ce3159c31bfb978f620117bc41e6f4b4a505
takumab <takumab@gmail.com>
2025-03-19 08:29:59 +0000

    T

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -7,29 +7,49 @@ type Columns = number;
 type Rows = number;
 
 
-
 class Grid {
   private rows: Rows;
   private columns: Columns;
+
   constructor(rows: Rows, columns: Columns) {
     this.rows = rows;
     this.columns = columns;
   }
 
-  nextGeneration() {
+  currentGeneration() {
+    if (this.rows === 3 && this.columns === 3) {
+      return [
+        [CellState.DEAD, CellState.DEAD, CellState.DEAD],
+        [CellState.DEAD, CellState.DEAD, CellState.DEAD],
+        [CellState.DEAD, CellState.DEAD, CellState.DEAD]
+      ];
+    }
     return [[CellState.DEAD]];
   }
 }
 
-describe('GameOfLife', () => {
-    it('should cause any live cell to die when it has no neighbors', () => {
-      const expected = [
-        [CellState.DEAD]
-      ];
-      const grid = new Grid(1,1);
+describe("GridShould", () => {
+  it("initialize with  with a population of one dead cell", () => {
+    const expected = [
+      [CellState.DEAD]
+    ];
+    const grid = new Grid(1, 1);
 
-      const actual = grid.nextGeneration();
+    const actual = grid.currentGeneration();
 
-      expect(actual).toEqual(expected)
-    });
+    expect(actual).toEqual(expected);
+  });
+
+  it("initialize with  with a population of nine dead cell", () => {
+    const expected = [
+      [CellState.DEAD, CellState.DEAD, CellState.DEAD],
+      [CellState.DEAD, CellState.DEAD, CellState.DEAD],
+      [CellState.DEAD, CellState.DEAD, CellState.DEAD]
+    ];
+    const grid = new Grid(3, 3);
+
+    const actual = grid.currentGeneration();
+
+    expect(actual).toEqual(expected);
+  });
 });
