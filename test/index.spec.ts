73f5dc3097a8a2c6910485bbc1a74923f17bf03c
takumab <takumab@gmail.com>
2025-03-19 09:23:33 +0000

    T

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -7,6 +7,21 @@ type Columns = number;
 type Rows = number;
 
 
+class Coordinates {
+  private x: number;
+  private y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  placeLivingCellOn(grid: CellState[][], livingCell: CellState) {
+    grid[this.x][this.y] = livingCell;
+    return grid;
+  }
+}
+
 class Grid {
   private rows: Rows;
   private columns: Columns;
@@ -20,6 +35,10 @@ class Grid {
 
   currentGeneration() {
     return this.grid;
+  }
+
+  addLivingCell(coordinates: Coordinates, livingCell: CellState) {
+    this.grid = coordinates.placeLivingCellOn(this.grid, livingCell);
   }
 }
 
@@ -46,6 +65,19 @@ describe("GridShould", () => {
 
     const actual = grid.currentGeneration();
 
+    expect(actual).toEqual(expected);
+  });
+
+  it("add a living cell on a 1x1 grid", () => {
+    const expected = [
+      [CellState.DEAD, CellState.ALIVE, CellState.DEAD]
+    ];
+    const grid = new Grid(1, 3);
+    const coordinates = new Coordinates(0, 1);
+
+    grid.addLivingCell(coordinates, CellState.ALIVE);
+
+    const actual = grid.currentGeneration();
     expect(actual).toEqual(expected);
   });
 });
